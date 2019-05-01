280b0296dcf0d95fca1ebf87769b5a5ce3406813
AyanaYanai <50027587+AyanaYanai@users.noreply.github.com>
2019-05-01 09:53:58 -0700

    Update LisaRunner.java

--- LisaRunner.java
+++ LisaRunner.java
@@ -6,14 +6,17 @@
 
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.font.TextAttribute;
 
 import javax.imageio.ImageIO;
 
 import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
+import java.text.AttributedString;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.color.*;
@@ -26,7 +29,7 @@ public class LisaRunner {
 	//private JTextArea chat = new JTextArea();
 	private Image bubble;
 	private Image imgs;
-	private String message="";
+	private AttributedString message;
 	
 	protected  Image getImage(String fn) {
 		Image img = null;
@@ -50,39 +53,65 @@ public class LisaRunner {
 		
 	}
 
-	private void start() {
-		imgs=getImage("house.jpg");
-		bubble=getImage("bubble.jpg");
+	public void start() {
+		imgs=getImage("house.png");
+		bubble=getImage("bubble.png");
 		JFrame frame = new JFrame("ChatBotRunner");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Font font = new Font("Lucida Handwriting",Font.PLAIN, 20);
+		AttributedString q = new AttributedString(lisa.askQuestion());
+		q.addAttribute(TextAttribute.FONT, font);	
+		message =q;
 		JPanel panel = new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {	
 				//super.paintComponents(g);
-				g.drawImage(imgs, 0,0,600,600,null);
-				g.drawImage(bubble,10,20,400,300,null);
-				lisa.draw(g,300,0);
-				g.drawString(message,10,20);
-				
+//				g.drawImage(imgs, 0,0,800,500,null);
+//				g.drawImage(bubble,150,20,550,450,null);
+//				lisa.draw(g,550,100);
+//				//g.drawString(message,250,100);
+//				g.drawString(message.getIterator(),250,100);
+				if(question()==true) {
+					g.drawImage(getImage("paper.jpg"),0,0, 500, 800, null);
+					g.drawString(message.getIterator(), 100, 200);
+				}
+				else {
+					g.drawImage(imgs, 0,0,800,500,null);
+					g.drawImage(bubble,150,20,550,450,null);
+					lisa.draw(g,550,100);
+					//g.drawString(message,250,100);
+					g.drawString(message.getIterator(),250,100);
+				}
 			}
 		};
-		textbox.setLocation(10,540);
-		textbox.setSize(580,30);
+	
+//		textbox.setLocation(10,450);
+//		textbox.setSize(790,30);
 		//chat.setLocation(10,50);
 		//chat.setSize(300,300);
-		panel.add(textbox);
+			
+//		panel.add(textbox);
 		//panel.add(chat);
-		panel.setPreferredSize(new Dimension(600,600));
-		panel.setSize(600,600);
-		frame.setResizable(false);
+//		panel.setPreferredSize(new Dimension(800,500));
+//		panel.setSize(600,600);
+		frame.setResizable(true);
 		panel.setVisible(true);
 		panel.setLayout(null);
+		if(question()==true) {
+			panel.setPreferredSize(new Dimension(500,800));
+		}
+		else {
+			panel.setPreferredSize(new Dimension(800,500));
+			textbox.setLocation(10,450);
+			textbox.setSize(790,30);
+			panel.add(textbox);
+		}
 		frame.add(panel);
 		frame.pack();
 		frame.setVisible(true);
 	//	chat.setText(lisa.askQuestion());
-		message =(lisa.askQuestion());
 		panel.repaint();
+		
 
 		/*private void nowAskQuestions() {
 			for(int i=0;i<7;i++) {
@@ -99,13 +128,27 @@ public class LisaRunner {
 				String text = textbox.getText();
 				lisa.saveResponse(text);
 				//chat.setText(lisa.askQuestion());
-				message= lisa.askQuestion();
+				Font font = new Font("Lucida Handwriting",Font.PLAIN, 20);
+				AttributedString q = new AttributedString(lisa.askQuestion());
+				q.addAttribute(TextAttribute.FONT, font);	
+				message =q;
+				//message= lisa.askQuestion();
 				panel.repaint();
 			  //  chat.append("You: " + text + "\n");
 				textbox.setText("");
+	
 			}
 			
 		});
+		
+	}
+
+
+	protected boolean question() {
+		if(lisa.getQuestionNum() == lisa.getQuestionList().size()+1) {
+			return true;
+		}
+		return false;
 	}
 
 /*	private void nowAskQuestions() {
