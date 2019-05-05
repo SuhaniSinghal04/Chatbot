aa0b7579f5fb4c0408b3f5f2e816c563bd26a9d1
SuhaniSinghal04 <50027733+SuhaniSinghal04@users.noreply.github.com>
2019-05-05 14:07:51 -0700

    Update Lisa.java

--- Lisa.java
+++ Lisa.java
@@ -1,32 +1,29 @@
-import java.awt.Font;
+
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.font.TextAttribute;
 import java.io.IOException;
-import java.text.AttributedString;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
 public class Lisa {
-	private ArrayList<String>catchphrases;
-	private ArrayList<String>responses;
-	private ArrayList<String> questionList;
-	private ArrayList<String> thesisList;
-	private static int questionNum;
-	private String question;
-	public final static String PATH_PREFIX = "res/images/";
-	private Image imgs;
+private ArrayList<String>catchphrases;
+private ArrayList<String>responses;
+private ArrayList<String> questionList;
+private ArrayList<String> thesisList;
+public static int questionNum;
+private String question;
+public final static String PATH_PREFIX = "res/images/";
+private Image imgs;
 
 protected  Image getImage(String fn) {
 	Image img = null;
-	//fn = PATH_PREFIX+fn;
+
 	try {
 		
 		img = ImageIO.read(this.getClass().getResource(fn));
 
 	} catch (IOException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
 	return img;
@@ -61,27 +58,24 @@ public String askQuestion() {
 	if(questionNum<questionList.size()) {
 	question= questionList.get(questionNum);
 	questionNum++;
-	
 	return question;
 	}
 	 makeThesis();
-	 questionNum++;
 	 return getThesisList();
 }
 
 public void makeThesis() {
-	thesisList.add("Even though "+ responses.get(4)+ ","+ responses.get(1)+"because "+ responses.get(2)+ " and "+ responses.get(3) );
-	thesisList.add(responses.get(1)+ " because "+ responses.get(2)+ " and "+ responses.get(3)+ ".");
+	thesisList.add("Even though "+ responses.get(4)+ ", "+ responses.get(1)+" because "+ responses.get(2)+ " and "+ responses.get(3) + ".");
+	thesisList.add(responses.get(1)+ " because "+ responses.get(2)+ " and "+ responses.get(3)+ "." );
 	thesisList.add("Whereas "+responses.get(4)+ ", "+ responses.get(1)+ " given that "+ responses.get(2)+ ".");
-	thesisList.add("Since "+ responses.get(2)+ " and "+ responses.get(3)+ ", "+ responses.get(1)+".");
-	thesisList.add(responses.get(1)+" even though "+ responses.get(4)+ ".");
+	thesisList.add("Since "+ responses.get(2)+ " and "+ responses.get(3)+ ", "+ responses.get(1)+"." );
+	thesisList.add(responses.get(1)+" even though "+ responses.get(4)+ "." );
 
 }
 
 
-
 public void draw(Graphics g, int x, int y) {
-	g.drawImage(imgs, x, y,200,400,null);
+	g.drawImage(imgs, x, y,300,600,null);
 }
 
 public void saveResponse(String s) {
@@ -105,9 +99,9 @@ public int getQuestionNum() {
 }
 
 public String getThesisList(){
-	String answer = "Here are some possible thesis options:  \n";
+	String answer = "Here are some possible thesis options:*";
 	for(int i=0; i<thesisList.size();i++) {
-		answer+= (i+1) + ".) " + thesisList.get(i)+"\n";
+		answer+= (i+1) + ".) " + thesisList.get(i)+"*";
 	}
 return answer;	
 }
@@ -115,6 +109,4 @@ return answer;
 
 	
 }
-
-
 
