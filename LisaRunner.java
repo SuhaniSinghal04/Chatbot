347ba7f4a0b71f852902c1e452d7a087ebcd80db
SuhaniSinghal04 <50027733+SuhaniSinghal04@users.noreply.github.com>
2019-05-05 14:07:19 -0700

    Update LisaRunner.java

--- LisaRunner.java
+++ LisaRunner.java
@@ -3,14 +3,12 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.font.TextAttribute;
 
 import javax.imageio.ImageIO;
 
 import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
-import java.text.AttributedString;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.color.*;
@@ -20,14 +18,28 @@ public class LisaRunner {
 	
 	private JTextField textbox = new JTextField();
 	private Lisa lisa= new Lisa();
-	//private JTextArea chat = new JTextArea();
 	private Image bubble;
 	private Image imgs;
-	private AttributedString message;
+	private Image notebook;
+	private String message1="";
+	private String message2="";
+	private String message3="";
+	private String lengthChecker="";
+	private String thesis1 = "";
+	private String thesis2 = "";
+	private String thesis3 = "";
+	private String thesis4 = "";
+	private String thesis5 = "";
+	private String thesis0 = "";
+	private String thesis15="";
+	private String thesis25="";
+	private String thesis35="";
+	private String thesis45="";
+	private String thesis55="";
 	
+	int x=0;
 	protected  Image getImage(String fn) {
 		Image img = null;
-		//fn = PATH_PREFIX+fn;
 		try {
 			
 			img = ImageIO.read(this.getClass().getResource(fn));
@@ -43,132 +55,138 @@ public class LisaRunner {
 	public static void main (String[] args) {
 	
 		new LisaRunner().start();
-		//new LisaRunner().nowAskQuestions();
+		
 		
 	}
 
-	public void start() {
-		imgs=getImage("house.png");
-		bubble=getImage("bubble.png");
-		JFrame frame = new JFrame("ChatBotRunner");
+	private void start() {
+		imgs=getImage("lisahouse.png");
+		bubble=getImage("bubble1.png");
+		notebook=getImage("notebook.png");
+		JFrame frame = new JFrame("Lisa");
+		JFrame frame2= new JFrame("Thesis");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Font font = new Font("Lucida Handwriting",Font.PLAIN, 20);
-		AttributedString q = new AttributedString(lisa.askQuestion());
-		q.addAttribute(TextAttribute.FONT, font);	
-		message =q;
+		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {	
-				//super.paintComponents(g);
-//				g.drawImage(imgs, 0,0,800,500,null);
-//				g.drawImage(bubble,150,20,550,450,null);
-//				lisa.draw(g,550,100);
-//				//g.drawString(message,250,100);
-//				g.drawString(message.getIterator(),250,100);
-				if(question()==true) {
-					g.drawImage(getImage("paper.jpg"),0,0, 500, 800, null);
-					g.drawString(message.getIterator(), 100, 200);
-				}
-				else {
-					g.drawImage(imgs, 0,0,800,500,null);
-					g.drawImage(bubble,150,20,550,450,null);
-					lisa.draw(g,550,100);
-					//g.drawString(message,250,100);
-					g.drawString(message.getIterator(),250,100);
-				}
+				super.paintComponents(g);
+				g.drawImage(imgs, 0,0,790,600,null);
+				g.drawImage(bubble,0,20,900,600,null);
+				lisa.draw(g,500,0);
+				Font myFont = new Font ("Lucida Handwriting", 1, 17);
+				g.setFont(myFont); 
+				g.drawString(message1,275,200);
+				g.drawString(message2, 275, 220);
+				g.drawString(message3, 275, 240);
+				
 			}
 		};
-	
-//		textbox.setLocation(10,450);
-//		textbox.setSize(790,30);
-		//chat.setLocation(10,50);
-		//chat.setSize(300,300);
-			
-//		panel.add(textbox);
-		//panel.add(chat);
-//		panel.setPreferredSize(new Dimension(800,500));
-//		panel.setSize(600,600);
-		frame.setResizable(true);
-		panel.setVisible(true);
-		panel.setLayout(null);
-		//if(question()==true) {
-		//	panel.setPreferredSize(new Dimension(500,800));
-		//}
-		//else {
-			panel.setPreferredSize(new Dimension(800,500));
-			textbox.setLocation(10,450);
-			textbox.setSize(790,30);
-			panel.add(textbox);
-		//}
-		frame.add(panel);
-		frame.pack();
-		frame.setVisible(true);
-	//	chat.setText(lisa.askQuestion());
-		panel.repaint();
 		
-
-		/*private void nowAskQuestions() {
-			for(int i=0;i<7;i++) {
-				System.out.print(lisa.questionNum);
+		JPanel panel2= new JPanel() {
+			@Override
+			public void paintComponent (Graphics g) {
+				g.drawImage(notebook, 0, 0, 790, 600, null);
+				Font a = new Font ("TimesRoman", Font.BOLD, 30);
+				g.setFont(a);
+				g.drawString(thesis0,105,50);
+				Font b = new Font ("TimesRoman", Font.PLAIN, 20);
+				g.setFont(b);
+				g.drawString(thesis1, 105, 80);
+				g.drawString(thesis15, 105, 105);
+				g.drawString(thesis2, 105, 130);
+				g.drawString(thesis25,105,155);
+				g.drawString(thesis3, 105, 175);
+				g.drawString(thesis35, 105, 200);
+				g.drawString(thesis4, 105, 225);
+				g.drawString(thesis45, 105, 250);
+				g.drawString(thesis5, 105, 275);
+				g.drawString(thesis55, 105, 300);
 				
-				chat.setText(lisa.askQuestion());
-				
-				lisa.saveResponse(textbox.getText());
 			}
-			*/
+		};
+		
+		textbox.setLocation(10,540);
+		textbox.setSize(580,30);
+		panel.add(textbox);
+		panel.setPreferredSize(new Dimension(790,600));
+		panel2.setPreferredSize(new Dimension(790,600));
+		frame.setResizable(false);
+		frame2.setResizable(true);
+		panel.setVisible(true);
+		panel2.setVisible(true);
+		panel.setLayout(null);
+		panel2.setLayout(null);
+		frame.add(panel);
+		frame2.add(panel2);
+		frame.pack();
+		frame2.pack();
+		frame.setVisible(true);
+		frame2.setVisible(false);
+		message1 =(lisa.askQuestion());
+		panel.repaint();
+		panel2.repaint();
+
+		
 		textbox.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String text = textbox.getText();
 				lisa.saveResponse(text);
-				//chat.setText(lisa.askQuestion());
-				Font font = new Font("Lucida Handwriting",Font.PLAIN, 20);
-				AttributedString q = new AttributedString(lisa.askQuestion());
-				q.addAttribute(TextAttribute.FONT, font);	
-				message =q;
+				lengthChecker= lisa.askQuestion();
+				if (lengthChecker.length()==19) {
+					message1= lengthChecker;
+				}
+				else if(lengthChecker.length()==46) {
+					message1=lengthChecker.substring(0,17);
+					message2=lengthChecker.substring(17,35);
+					message3=lengthChecker.substring(35,46);
+				}
+				else if(lengthChecker.length()==50) {
+					message1=lengthChecker.substring(0,21);
+					message2=lengthChecker.substring(21,39);
+					message3=lengthChecker.substring(39,50);
+				}
+				else if (lengthChecker.length()==49) {
+					message1=lengthChecker.substring(0,17);
+					message2=lengthChecker.substring(17,34);
+					message3=lengthChecker.substring(34,49);
+				}
+				else {
+				x= lengthChecker.indexOf("*");
+				thesis0= lengthChecker.substring(0,x);
+				lengthChecker=lengthChecker.substring(x+1);
+				x= lengthChecker.indexOf("*");
+				thesis1= lengthChecker.substring(0,x);
+				if(thesis1.length()>75) {
+					thesis15=thesis1.substring(76);
+					thesis1=thesis1.substring(0,76);
+				}
+				lengthChecker=lengthChecker.substring(x+1);
+				x= lengthChecker.indexOf("*");
+				thesis2= lengthChecker.substring(0,x);
+				lengthChecker=lengthChecker.substring(x+1);
+				x= lengthChecker.indexOf("*");
+				thesis3= lengthChecker.substring(0,x);
+				lengthChecker=lengthChecker.substring(x+1);
+				x= lengthChecker.indexOf("*");
+				thesis4= lengthChecker.substring(0,x);
+				lengthChecker=lengthChecker.substring(x+1);
+				x= lengthChecker.indexOf("*");
+				thesis5= lengthChecker.substring(0,x);
+				lengthChecker=lengthChecker.substring(x+1);
+				
+                   frame2.setVisible(true);
+                   frame.dispose();
+				}
 				panel.repaint();
-			  //  chat.append("You: " + text + "\n");
+				panel2.repaint();
 				textbox.setText("");
-	
 			}
 			
 		});
-		
 	}
 
-
-	protected boolean question() {
-		if(lisa.getQuestionNum() == lisa.getQuestionList().size()+1) {
-			return true;
-		}
-		return false;
-	}
-
-/*	private void nowAskQuestions() {
-		for(int i=0;i<7;i++) {
-			System.out.print(lisa.questionNum);
-			
-			chat.setText(lisa.askQuestion());
-			
-			lisa.saveResponse(textbox.getText());
-		}
-	*/	
 		
-		
-			//System.out.println(lisa.askQuestion());
-			/*lisa.saveResponse("health Effects of Music");
-			System.out.println(lisa.askQuestion());
-			lisa.saveResponse("listening to favorite music is good for health");
-			System.out.println(lisa.askQuestion());
-			lisa.saveResponse("music reduces stresss");
-			System.out.println(lisa.askQuestion());
-			lisa.saveResponse("trials revelaed that it relieves pain");
-			System.out.println(lisa.askQuestion());
-			lisa.saveResponse("invidiuals can respond diferently to the same piece of music ");
-			System.out.println(lisa.askQuestion());
-			*/
-		}
-	
-
-	
+}
 
